9abd3a76883558836ad25d7d72b4a74303eab3cc
strangeingenuity <mail_tyaga@yahoo.com>
2017-10-13 09:28:30 -0600

    added changes to the static parameter 11

--- MakeMeMoneyThisQuarter/settings.py
+++ MakeMeMoneyThisQuarter/settings.py
@@ -119,12 +119,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#)
+# commented this out to test on server STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
+# this was commented earlier '/var/www.static',
+#] this also
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-# commented this out to test on server STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
-# this was commented earlier '/var/www.static',
-#] this also
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

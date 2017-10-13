7571120c850fada8ecc2ae039f66d4c67602b178
strangeingenuity <mail_tyaga@yahoo.com>
2017-10-13 09:21:13 -0600

    added static file changes

--- MakeMeMoneyThisQuarter/settings.py
+++ MakeMeMoneyThisQuarter/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -118,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
-#'/var/www.static',
-]
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+# commented this out to test on server STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
+# this was commented earlier '/var/www.static',
+#] this also
